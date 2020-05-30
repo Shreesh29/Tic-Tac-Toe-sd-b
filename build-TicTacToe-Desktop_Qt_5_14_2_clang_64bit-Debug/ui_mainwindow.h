3cd6dd2e1da805be5e1d24959a17a225554bf24d
Shreesh29 <Shreesh29@hotmail.com>
2020-05-30 22:03:28 +0530

    Added background and finished UI Design

--- build-TicTacToe-Desktop_Qt_5_14_2_clang_64bit-Debug/ui_mainwindow.h
+++ build-TicTacToe-Desktop_Qt_5_14_2_clang_64bit-Debug/ui_mainwindow.h
@@ -77,44 +77,70 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(800, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-image: url(:/Background.jpg);"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
         label->setGeometry(QRect(340, 130, 131, 81));
+        label->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_2 = new QLabel(centralwidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
         label_2->setGeometry(QRect(480, 130, 131, 81));
+        label_2->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_3 = new QLabel(centralwidget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
         label_3->setGeometry(QRect(610, 130, 131, 81));
+        label_3->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         label_4 = new QLabel(centralwidget);
         label_4->setObjectName(QString::fromUtf8("label_4"));
         label_4->setGeometry(QRect(340, 220, 131, 81));
+        label_4->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_5 = new QLabel(centralwidget);
         label_5->setObjectName(QString::fromUtf8("label_5"));
         label_5->setGeometry(QRect(480, 220, 131, 81));
+        label_5->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_6 = new QLabel(centralwidget);
         label_6->setObjectName(QString::fromUtf8("label_6"));
         label_6->setGeometry(QRect(610, 220, 131, 81));
+        label_6->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         label_7 = new QLabel(centralwidget);
         label_7->setObjectName(QString::fromUtf8("label_7"));
         label_7->setGeometry(QRect(340, 320, 131, 81));
+        label_7->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_8 = new QLabel(centralwidget);
         label_8->setObjectName(QString::fromUtf8("label_8"));
         label_8->setGeometry(QRect(480, 320, 131, 81));
+        label_8->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_9 = new QLabel(centralwidget);
         label_9->setObjectName(QString::fromUtf8("label_9"));
         label_9->setGeometry(QRect(610, 320, 131, 81));
+        label_9->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         line = new QFrame(centralwidget);
         line->setObjectName(QString::fromUtf8("line"));
         line->setGeometry(QRect(410, 130, 61, 271));
+        line->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         line->setLineWidth(3);
         line->setFrameShape(QFrame::VLine);
         line->setFrameShadow(QFrame::Sunken);
         line_2 = new QFrame(centralwidget);
         line_2->setObjectName(QString::fromUtf8("line_2"));
         line_2->setGeometry(QRect(560, 130, 61, 271));
+        line_2->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         line_2->setLineWidth(3);
         line_2->setFrameShape(QFrame::VLine);
         line_2->setFrameShadow(QFrame::Sunken);
@@ -122,6 +148,9 @@ public:
         line_3->setObjectName(QString::fromUtf8("line_3"));
         line_3->setGeometry(QRect(310, 210, 411, 20));
         line_3->setAutoFillBackground(false);
+        line_3->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         line_3->setLineWidth(3);
         line_3->setFrameShape(QFrame::HLine);
         line_3->setFrameShadow(QFrame::Sunken);
@@ -129,89 +158,124 @@ public:
         line_4->setObjectName(QString::fromUtf8("line_4"));
         line_4->setGeometry(QRect(310, 300, 411, 20));
         line_4->setAutoFillBackground(false);
+        line_4->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         line_4->setLineWidth(3);
         line_4->setFrameShape(QFrame::HLine);
         line_4->setFrameShadow(QFrame::Sunken);
         label_11 = new QLabel(centralwidget);
         label_11->setObjectName(QString::fromUtf8("label_11"));
-        label_11->setGeometry(QRect(420, 200, 58, 16));
-        label_11->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_11->setGeometry(QRect(420, 200, 16, 16));
+        label_11->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_12 = new QLabel(centralwidget);
         label_12->setObjectName(QString::fromUtf8("label_12"));
-        label_12->setGeometry(QRect(570, 200, 58, 16));
-        label_12->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_12->setGeometry(QRect(570, 200, 16, 16));
+        label_12->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_13 = new QLabel(centralwidget);
         label_13->setObjectName(QString::fromUtf8("label_13"));
-        label_13->setGeometry(QRect(700, 200, 58, 16));
-        label_13->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_13->setGeometry(QRect(700, 200, 16, 16));
+        label_13->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         label_14 = new QLabel(centralwidget);
         label_14->setObjectName(QString::fromUtf8("label_14"));
-        label_14->setGeometry(QRect(420, 290, 58, 16));
-        label_14->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_14->setGeometry(QRect(420, 290, 16, 16));
+        label_14->setAutoFillBackground(false);
+        label_14->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_15 = new QLabel(centralwidget);
         label_15->setObjectName(QString::fromUtf8("label_15"));
-        label_15->setGeometry(QRect(570, 290, 58, 16));
-        label_15->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_15->setGeometry(QRect(570, 290, 16, 16));
+        label_15->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_16 = new QLabel(centralwidget);
         label_16->setObjectName(QString::fromUtf8("label_16"));
-        label_16->setGeometry(QRect(700, 290, 58, 16));
-        label_16->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_16->setGeometry(QRect(700, 290, 16, 16));
+        label_16->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         label_17 = new QLabel(centralwidget);
         label_17->setObjectName(QString::fromUtf8("label_17"));
-        label_17->setGeometry(QRect(420, 380, 58, 16));
-        label_17->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_17->setGeometry(QRect(420, 380, 16, 16));
+        label_17->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_18 = new QLabel(centralwidget);
         label_18->setObjectName(QString::fromUtf8("label_18"));
-        label_18->setGeometry(QRect(570, 380, 58, 16));
-        label_18->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_18->setGeometry(QRect(570, 380, 16, 16));
+        label_18->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_19 = new QLabel(centralwidget);
         label_19->setObjectName(QString::fromUtf8("label_19"));
-        label_19->setGeometry(QRect(700, 380, 58, 16));
-        label_19->setStyleSheet(QString::fromUtf8("font: bold;"));
+        label_19->setGeometry(QRect(700, 380, 16, 16));
+        label_19->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;\n"
+""));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
         pushButton_2->setGeometry(QRect(110, 250, 71, 31));
-        pushButton_2->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_2->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_3 = new QPushButton(centralwidget);
         pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
         pushButton_3->setGeometry(QRect(190, 250, 71, 31));
-        pushButton_3->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_3->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_4 = new QPushButton(centralwidget);
         pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
         pushButton_4->setGeometry(QRect(110, 290, 71, 31));
-        pushButton_4->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_4->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_5 = new QPushButton(centralwidget);
         pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
         pushButton_5->setGeometry(QRect(190, 290, 71, 31));
-        pushButton_5->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_5->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_6 = new QPushButton(centralwidget);
         pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
         pushButton_6->setGeometry(QRect(110, 330, 71, 31));
-        pushButton_6->setStyleSheet(QString::fromUtf8("font: bold\n"
-";"));
+        pushButton_6->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_7 = new QPushButton(centralwidget);
         pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
         pushButton_7->setGeometry(QRect(190, 330, 71, 31));
-        pushButton_7->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_7->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_8 = new QPushButton(centralwidget);
         pushButton_8->setObjectName(QString::fromUtf8("pushButton_8"));
         pushButton_8->setGeometry(QRect(110, 370, 71, 31));
-        pushButton_8->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_8->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_9 = new QPushButton(centralwidget);
         pushButton_9->setObjectName(QString::fromUtf8("pushButton_9"));
         pushButton_9->setGeometry(QRect(190, 370, 71, 31));
-        pushButton_9->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_9->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         pushButton_10 = new QPushButton(centralwidget);
         pushButton_10->setObjectName(QString::fromUtf8("pushButton_10"));
         pushButton_10->setGeometry(QRect(150, 410, 71, 31));
-        pushButton_10->setStyleSheet(QString::fromUtf8("font: bold;"));
+        pushButton_10->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color:white;\n"
+""));
         label_22 = new QLabel(centralwidget);
         label_22->setObjectName(QString::fromUtf8("label_22"));
         label_22->setGeometry(QRect(150, 200, 32, 20));
+        label_22->setStyleSheet(QString::fromUtf8("color:white;"));
         start_game = new QPushButton(centralwidget);
         start_game->setObjectName(QString::fromUtf8("start_game"));
         start_game->setGeometry(QRect(90, 40, 191, 61));
         start_game->setStyleSheet(QString::fromUtf8("\n"
+"color:white;\n"
 "font: 75 18pt \"Chalkboard SE\";"));
         layoutWidget = new QWidget(centralwidget);
         layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
@@ -236,19 +300,60 @@ public:
         label_10 = new QLabel(centralwidget);
         label_10->setObjectName(QString::fromUtf8("label_10"));
         label_10->setGeometry(QRect(130, 220, 161, 20));
+        label_10->setStyleSheet(QString::fromUtf8("color:white;"));
         winner = new QLabel(centralwidget);
         winner->setObjectName(QString::fromUtf8("winner"));
         winner->setGeometry(QRect(210, 450, 511, 51));
-        winner->setStyleSheet(QString::fromUtf8("color:red;\n"
+        winner->setStyleSheet(QString::fromUtf8("color:white;\n"
+"font:bold;\n"
 "font: 24pt \"Arial\";"));
         turn = new QLabel(centralwidget);
         turn->setObjectName(QString::fromUtf8("turn"));
         turn->setGeometry(QRect(190, 200, 132, 20));
-        turn->setStyleSheet(QString::fromUtf8("font: 13pt \"Chalkboard SE\";"));
+        turn->setStyleSheet(QString::fromUtf8("font: bold;\n"
+"color: white;"));
         label_23 = new QLabel(centralwidget);
         label_23->setObjectName(QString::fromUtf8("label_23"));
         label_23->setGeometry(QRect(370, 20, 461, 81));
         MainWindow->setCentralWidget(centralwidget);
+        label_6->raise();
+        label_9->raise();
+        label_3->raise();
+        label_8->raise();
+        label_5->raise();
+        label_2->raise();
+        label->raise();
+        label_7->raise();
+        label_4->raise();
+        line_2->raise();
+        line->raise();
+        line_4->raise();
+        line_3->raise();
+        label_10->raise();
+        turn->raise();
+        label_22->raise();
+        label_13->raise();
+        label_16->raise();
+        label_19->raise();
+        pushButton_2->raise();
+        pushButton_3->raise();
+        pushButton_4->raise();
+        pushButton_5->raise();
+        pushButton_6->raise();
+        pushButton_7->raise();
+        pushButton_8->raise();
+        pushButton_9->raise();
+        pushButton_10->raise();
+        start_game->raise();
+        layoutWidget->raise();
+        winner->raise();
+        label_23->raise();
+        label_12->raise();
+        label_15->raise();
+        label_18->raise();
+        label_14->raise();
+        label_17->raise();
+        label_11->raise();
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 800, 22));
